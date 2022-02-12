10358e0ee84ad687ad68db33f543fd0740c0e07b
mynameissk <41878703+mynameissk@users.noreply.github.com>
2022-02-12 17:33:13 +0300

    Add files via upload

---
+++ ALLAH_SAVAR.py
@@ -0,0 +1,322 @@
+# BÜTÜN KAYNAK KODLARI SEZER KARAOĞLAN'A AİTTİR
+
+import os
+os.system("")
+from time import sleep
+import colorama
+from colorama import Fore, init
+start = "\033[1;95m"
+end = "\033[0;0m"
+
+print(Fore.RESET + "Renk Paketleri Yükleniyor")
+
+
+
+
+
+sleep(2)
+
+COLOR = {
+    "HEADER": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[91m",
+    "ENDC": "\033[0m",
+}
+print(COLOR["BLUE"], "Mavi Test Edildi", COLOR["ENDC"])
+sleep(1)
+print(COLOR["RED"], "Kırmızı Test Edildi", COLOR["ENDC"])
+sleep(1)
+print(COLOR["GREEN"], "Yeşil Test Edildi", COLOR["ENDC"])
+sleep(2)
+print(Fore.LIGHTBLUE_EX + "Powered By SEZER ")
+sleep(2)
+print("...")
+print(Fore.RESET + "KAYNAK PAKETLER YÜKLENİYOR")
+print(COLOR["GREEN"], "OK", COLOR["ENDC"])
+sleep(1)
+print(COLOR["GREEN"], " OK", COLOR["ENDC"])
+sleep(1)
+print(COLOR["GREEN"], "  OK", COLOR["ENDC"])
+sleep(2)
+print(COLOR["GREEN"], "   OK", COLOR["ENDC"])
+sleep(1)
+print(COLOR["GREEN"], "    OK", COLOR["ENDC"])
+sleep(2)
+print(COLOR["GREEN"], "     OK", COLOR["ENDC"])
+cevap1 = "Evet"
+cevap2 = "evet"
+cevap3 = "EVET"
+cevap4 = "tm"
+cevap5 = "Tm"
+cevap6 = "Sikilir"
+cevap7 = "sikilir"
+cevap8 = "SİKİLİR"
+
+
+
+
+
+
+init(autoreset=True)
+# BÜTÜN KAYNAK KODLARI SEZER KARAOĞLAN'A AİTTİR
+print(Fore.RED + "Program Sezer tarafından yapılmıştır emeği geçen herkesin allahını götünden sikeyim.")
+print("                                    C* ")
+print("                                    |*  ")
+print("                                   / \* ")
+print("                                  /   \* ")
+print("                       |          [===] ")
+print("                      / \          I I ")
+print("                   .-'    '-.      |_|  ")
+print("                 .' .        `.   (___)")
+print("               .'.' .':`. `.`.`.   | |")
+print("              /  :  :  :  :  :  \  | |")
+print("            __IIIOIIIOIIIOIIIOIII__| |")
+print("           T^^^^^^^^^^^^^^^^^^^^^^^| | ")
+print("           |   _      ___      _   | | ")
+print("           |  /^\   .' | `.   /^\  | | ")
+print("           |  |_| :/   |   \: |_|  | | ")
+print("           |      :|   |   |:      | | ")
+print("           |......:|  c|c  |:......I I ")
+print("           |======:|___|___|:======|_| ")
+print("            ------/_________\------ ")
+print("                   ---------")
+
+
+
+
+print(Fore.BLUE + "Aşağıda ki Soruyu Cevapla")
+print("")
+print("")
+print(Fore.GREEN + "ALLAH SİKİLİR Mİ? : ", end=' ')
+# BÜTÜN KAYNAK KODLARI SEZER KARAOĞLAN'A AİTTİR
+
+cevap_ver = input()
+if cevap_ver == cevap1 or cevap_ver == cevap2 or cevap_ver == cevap3 or cevap_ver == cevap4 or cevap_ver == cevap5 or cevap_ver == cevap6 or cevap_ver == cevap7 or cevap_ver == cevap8:
+    print(Fore.RED + "OFFF DUR DAHA FAZLA KONUŞMA SİKİM KALKTI")
+    print("")
+    print(Fore.RED + "Powered By Sezer Karaoğlan")
+else:
+    # BÜTÜN KAYNAK KODLARI SEZER KARAOĞLAN'A AİTTİR
+    print(Fore.GREEN + cevap_ver, Fore.RED + "NE DEMEK OLM",
+          "EVET DİYCEN KAHPE EVLADI ALLAHINI SİKEYİM OK? YAY? SIJI MIJI AMINA KOYIM")
+
+
+
+
+sleep(3)
+print(Fore.BLUE + "``````````````````````````````````````````````````")
+print(Fore.BLUE + "``````````````````````````````````````````````````")
+print(Fore.BLUE + "``````````````````````````````````````````````↑↑``")
+print(Fore.BLUE + "``````````````````````````$````````````````↑$§$↑``")
+print(Fore.BLUE + "``````````````````````````¶§```````````↑¶$§$↑`↑§``")
+print(Fore.BLUE + "``````````````````````````↑¶```````↑$¶¶$$````↑¶¶``")
+print(Fore.BLUE + "```````````````````````````¶`````↑¶¶↑↑```````$¶¶↑`")
+print(Fore.BLUE + "```````````````````````````↑`````↑```````````¶§¶¶`")
+print(Fore.BLUE + "```````````````````````````$`↑¶``````````````¶¶↑↑`")
+print(Fore.BLUE + "```````````````````````$↑``¶``¶``````````````¶¶↑``")
+print(Fore.BLUE + "```````````````````````↑¶`↑¶$`¶``````````````¶¶↑``")
+print(Fore.BLUE + "````````````````````````$¶¶↑¶¶$``````````````§¶↑``")
+print(Fore.BLUE + "```````````````````````````````````↑`````````¶¶↑``")
+print(Fore.BLUE + "```````````````````````````$↑``````¶$````````¶¶↑``")
+print(Fore.BLUE + "````````````````↑`````````↑¶$`````$¶¶$```````§¶$``")
+print(Fore.BLUE + "``````````````$¶¶`````````↑¶↑`````↑¶$¶↑``````¶¶↑``")
+print(Fore.BLUE + "``````````````¶¶¶↑````````↑¶¶``````¶¶¶¶↑`````§¶↑``")
+print(Fore.BLUE + "``↑`````````↑$``¶↑````````↑¶¶``````↑¶¶↑``````$¶↑``")
+print(Fore.BLUE + "``↑`````````¶§``¶``````````¶$```````$¶$``````$¶↑``")
+print(Fore.BLUE + "`↑↑````````¶§``↑¶``````````¶¶↑```````¶¶¶`````$¶↑``")
+print(Fore.BLUE + "`↑↑```````↑¶```↑¶``````````$¶$```````↑¶¶↑````$¶↑``")
+print(Fore.BLUE + "``↑```````¶↑```↑¶``````````↑§↑````````↑$$````$¶↑``")
+print(Fore.BLUE + "``§``````$¶`````¶↑`````````↑¶¶$````````¶¶````↑¶```")
+print(Fore.BLUE + "``¶``````¶``````¶¶````````↑¶`↑¶````````$↑````↑¶```")
+print(Fore.BLUE + "``§§```↑¶↑``````↑¶§↑↑`↑↑↑¶¶§``¶¶↑↑```↑$$`````$¶```")
+print(Fore.BLUE + "```↑¶¶¶¶¶````````$¶¶¶¶¶↑§¶§```¶¶¶↑$¶¶¶¶$`````$§```")
+print(Fore.BLUE + "```↑¶¶$``````````↑$§¶¶$$↑`````↑¶$¶¶¶¶$``````¶↑```")
+print(Fore.BLUE + "`````````````````````````````````↑↑↑↑````````↑````")
+print(Fore.BLUE + "``````````````````````````````````````````````````")
+from time import sleep
+sleep(1)
+print(Fore.WHITE +     "                                             █         █                                   ")
+print(Fore.MAGENTA +   "─█▀▀█ ░█─── ░█─── ─█▀▀█ ░█─░█ ▀█▀ 　 ░█▀▀▀█ ▀█▀ ░█─▄▀ ▀█▀ ░█──░█ ░█▀▀▀█ ░█▀▀█ ░█─░█ ░█▀▄▀█ ")
+print(Fore.CYAN +      "░█▄▄█ ░█─── ░█─── ░█▄▄█ ░█▀▀█ ░█─ 　 ─▀▀▀▄▄ ░█─ ░█▀▄─ ░█─ ░█▄▄▄█ ░█──░█ ░█▄▄▀ ░█─░█ ░█░█░█ ")
+print(Fore.YELLOW +    "░█─░█ ░█▄▄█ ░█▄▄█ ░█─░█ ░█─░█ ▄█▄　  ░█▄▄▄█ ▄█▄ ░█─░█ ▄█▄ ──░█── ░█▄▄▄█ ░█─░█ ─▀▄▄▀ ░█──░█ ")
+
+sleep(3)
+print("")
+print("ALLAH'IN 4 POSTA SİKİLMESİ UYGUN GÖRÜLMÜŞTÜR.")
+
+print("")
+
+
+def yes_no(question_to_be_answered):
+    while True:
+        choice = input(question_to_be_answered).lower()
+        if choice[:1] == 'e' :
+            return True
+        elif choice[:1] == 'h' :
+            return False
+        else:
+            print(Fore.GREEN + "'Evet'" + Fore.BLUE + "/" + Fore.RED + "'Hayır' " + Fore.BLUE + "Yada"  + Fore.GREEN + "'E'" + Fore.BLUE + "/" + Fore.RED + "'h'" + Fore.MAGENTA + "Şeklinde Yanıtlayınız.")
+
+#See it in Practice below
+
+musical_taste = yes_no(Fore.YELLOW + "'ALLAH 4 POSTA SİKİLECEKTİR KABUL EDİYORSAN '" + Fore.GREEN + "'E'" + Fore.RESET + "'ETMİYORSAN'" + Fore.RED + "'h'" + Fore.RESET + "'YE BAS")
+print("iSTEMESEN DE BU ARTIK KAÇINILMAZ SON...")
+if musical_taste == True:
+    print('ALLAH SİKİLMEYE BAŞLANIYOR...')
+elif musical_taste == False:
+    print('SENİN BEN ALLAHINI PEYGAMBERİNİ SİKEYİM')
+    sleep(3)
+    quit()
+
+
+print(Fore.LIGHTBLUE_EX +     "ALLAHI SİKME YÜZDEM")
+from time import sleep
+from tqdm import tqdm
+for c in tqdm(range(4)):
+    sleep(1)
+
+
+
+import datetime, time
+then = datetime.datetime.now() + datetime.timedelta(seconds=4)
+while then > datetime.datetime.now():
+    print ('ALLAHA İLK POSTA ATILDI')
+    time.sleep(1)
+    print ("ALLAHA İKİNCİ POSTA ATILDI")
+    time.sleep(1)
+    print ("ALLAHA ÜÇÜNCÜ POSTA ATILDI")
+    time.sleep(1)
+    print ("ALLAHIN AMINA ATILACAK SON POSTA BEKLENİYOR - Tahmini süre:0:10 Saniye")
+    time.sleep(1)
+    print("ALLAHIN AMINA ATILACAK SON POSTA BEKLENİYOR  ---- Tahmini süre:0:07 Saniye")
+    time.sleep(1)
+    print("ALLAHIN AMINA ATILACAK SON POSTA BEKLENİYOR  ------------ Tahmini süre:0:04 Saniye")
+    time.sleep(1)
+    print("ALLAHIN AMINA ATILACAK SON POSTA BEKLENİYOR  ----------------------------- Tahmini süre:0:2 Saniye")
+    time.sleep(1)
+    print("ALLAHIN AMINA ATILACAK SON POSTA BEKLENİYOR  **************************************Tahmini süre:0:00 Tamamlanıyor")
+    time.sleep(1)
+    print("ALLAHA DÖRDÜNCÜ POSTA ATILDI")
+    print("İŞLEM BAŞARIYLA TAMAMLANDI.")
+    time.sleep(2)
+    print("ALLAH DİSCONNECT SERVER FROM SERVER SIJI MIJI AMINA KOYIM HERİF ÖLDÜ AMINA KOYIM")
+    time.sleep(2)
+    print("KIZ KIZA TAMAM DA ERKEK ERKEĞE TAMAM DA ALLAH PEYGAMBERE DE TAMAM")
+    time.sleep(3)
+    print("")
+    print("")
+    time.sleep(1)
+    print("KÜRT KARDEŞLERİMİZİ DE UNUTMUYORUZ")
+    time.sleep(1)
+    print("KÜRT SİMİLASYONU BAŞLIYOR o")
+    time.sleep(1)
+    print("KÜRT SİMİLASYONU BAŞLIYOR oo")
+    time.sleep(1)
+    print("KÜRT SİMİLASYONU BAŞLIYOR ooo")
+    time.sleep(1)
+    print("KÜRT SİMİLASYONU BAŞLIYOR oooo ")
+    time.sleep(1)
+    print (Fore.GREEN +"KÜRT SİMİLASYONU BAŞARIYLA YÜKLENDİ")
+    time.sleep(2)
+    print(Fore.RESET +"BEKLEYİNİZ...")
+    time.sleep(3)
+
+
+
+print(Fore.RESET + "")
+print(Fore.RESET + "                                               ██                                  ██        ")
+print(Fore.RESET + "                                              ██▓▓██                              ██▓▓▓▓ ")
+print(Fore.RESET + "                                              ██▓▓██                              ██▓▓▓▓")
+print(Fore.RESET + "                                            ██▓▓▓▓██                              ██▓▓▓▓██")
+print(Fore.RESET + "                                            ██▓▓▒▒▓▓██                          ██▓▓▒▒▓▓██")
+print(Fore.RESET + "                                            ██▓▓▒▒▒▒▓▓▓▓                      ▓▓▓▓▒▒▒▒▓▓██")
+print(Fore.RESET + "                                           ██▓▓▒▒▒▒▓▓██                      ██▒▒▒▒▒▒▓▓██")
+print(Fore.RESET + "                                           ██▒▒▒▒▒▒▒▒██                      ██▒▒▓▓▒▒▓▓██")
+print(Fore.RESET + "                                          ██▒▒▓▓▓▓▒▒██                      ██▓▓▓▓▒▒▓▓██")
+print(Fore.RESET + "                                          ░░██▓▓▓▓▓▓▓▓▓▓                  ▓▓▓▓▓▓▓▓▓▓▓▓░░")
+print(Fore.RESET + "                                       ██▒▒▓▓▓▓██▒▒██              ██▒▒██▓▓▓▓▒▒▓▓")
+print(Fore.RESET + "                                       ██▒▒▓▓▓▓██████    ░░▒▒░░    ██████▓▓▓▓▒▒▓▓")
+print(Fore.RESET + "                                       ░░██▓▓████████▓▓▒▒▒▒▓▓▒▒▒▒▓▓████████▓▓██")
+print(Fore.RESET + "                                           ██▒▒████▓▓██▓▓██▓▓██▓▓██▓▓████▒▒██")
+print(Fore.RESET + "                                             ██▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓██")
+print(Fore.RESET + "                                               ████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████")
+print(Fore.RESET + "                                               ████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████")
+print(Fore.RESET + "                                               ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                               ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+print(Fore.RESET + "                                               ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                             ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+print(Fore.RESET + "                                             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+print(Fore.RESET + "                                           ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                           ██░░░░██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██░░░░██")
+print(Fore.RESET + "                                             ▓▓░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░▓▓")
+print(Fore.RESET + "                                             ██▓▓░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░▒▒██")
+print(Fore.RESET + "                                             ▒▒██░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██░░██▒▒")
+print(Fore.RESET + "                                             ██░░▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██░░▓▓")
+print(Fore.RESET + "                                             ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▓▓▓▓▓")
+print(Fore.RESET + "                                             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+print(Fore.RESET + "                                             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+print(Fore.RESET + "                                             ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                               ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                               ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                               ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                               ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██")
+print(Fore.RESET + "                                                 ▓▓▓▓▓▓▓▓▒▒░░▒▒▓▓▓▓▓▓██")
+print(Fore.RESET + "                                                 ▓▓▓▓██          ▓▓▓▓██")
+print(Fore.RESET + "                                                 ▓▓▓▓██        ░░▓▓▓▓██")
+print(Fore.RESET + "                                                 ▓▓██            ░░▓▓██")
+print(Fore.RESET + "                                                 ▓▓██            ░░▓▓██")
+print(Fore.RESET + "                                                 ░░░░              ░░░░")
+print(Fore.RESET + "                                                 ░░▒▒▒▒    ░░  ░░▒▒▒▒▒▒")
+print(Fore.RESET + "                                                 ▒▒██▒▒░░░░░░░░▒▒▓▓██▒▒")
+print(Fore.RESET + "                                                 ▒▒████▒▒▒▒▒▒▒▒▓▓████▒▒")
+print(Fore.RESET + "                                                 ▒▒▓▓██▓▓▓▓▓▓▓▓▓▓██▒▒▒▒")
+print(Fore.RESET + "                                                   ▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▒▒")
+print(Fore.RESET + "                                                     ▒▒▓▓▓▓▓▓▓▓▒▒▒▒")
+print(Fore.RESET + "                                                     ░░▒▒▒▒▒▒▒▒▒▒")
+print(Fore.RESET + "                                                       ░░░░░░░░░░")
+print("")
+
+time.sleep(3)
+print(Fore.LIGHTBLUE_EX + "APONUN EŞŞŞEĞİ SİKİLMELİK")
+
+print(Fore.LIGHTBLUE_EX + "AMINA KODUMUN EŞŞEĞİ BANA AKIL VERİYOR SİK KIRIĞINA BAK")
+print("5 sn. Bekle amına kodumun eşşeği")
+time.sleep(5)
+
+print(Fore.RESET + " ' ..... ▄▄ ▄▄   '"                       + Fore.RESET + "   　           ▄█▀█▀█▄")
+print(Fore.RESET + "'......▄▌▒▒▀▒▒▐▄  '"                      + Fore.RESET + "   　　　　 　▄█▀　　█　　▀█▄")
+print(Fore.RESET + "'.... ▐▒▒▒▒▒▒▒▒▒▌  '"                     + Fore.RESET + "　　   　　 ▄█▀　　　　　　　▀█▄'")
+print(Fore.RESET + "'... ▐▒▒▒▒▒▒▒▒▒▒▒▌ '"                     + Fore.RESET + "　　　     █　　　　　　　　　　　█'")
+print(Fore.RESET + "'....▐▒▒▒▒▒▒▒▒▒▒▒▌   '"                   + Fore.RESET + "          █　　　　　　　　　　　█'")
+print(Fore.RESET + "'....▐▀▄▄▄▄▄▄▄▄▄▀▌  '"                    + Fore.RESET + "            ▀█▄▄　　█　　　▄█▀'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌  '"                    + Fore.RESET + "              █　　▄▀▄　　█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌   '"                   + Fore.RESET + "              █　▀　　　▀　█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌   '"                   + Fore.RESET + "              █　　　　　　　█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌   '"                   + Fore.RESET + "              █　　　　　　　█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌    '"                  + Fore.RESET + "'              █　　　　 　　 █'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌    '"                  + Fore.RESET + "'              █　　　　　　　█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌    '"                  + Fore.RESET + "'              █　　　　　　　█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌    '"                  + Fore.RESET + "'        ▄█▀▀█▄█　　　　　　　█▄█▀█▄▄▄▄'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌     '"                 + Fore.RESET + "'     █▀▀　　　　▀　　　　　　　　　　　　▀▀█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌     '"                 + Fore.RESET + "'  ▄█▀▀　　　　▀　　　　　　　　　　　　   ▀▀█'")
+print(Fore.RESET + "'....▐░░░░░░░░░░░▌     '"                 + Fore.RESET + "'  █▀　　　　　　　　　　　　　　　　　　　　　　▀█'")
+print(Fore.RESET + "'...▄█▓░░░░░░░░░▓█▄    '"                 + Fore.RESET + "'  █　　　　　　　　　　　　　　　　　　　　　　　　█'")
+print(Fore.RESET + "'..▄▀░░░░░░░░░░░░░ ▀▄   '"                + Fore.RESET + "'  █　　　　　　　　　　　▄█▄　　　　　　　　　　█'")
+print(Fore.RESET + "'.▐░░░░░░░▀▄▒▄▀░░░░░░▌  '"                + Fore.RESET + "'   ▀█　　　　　　　　　█▀　▀█　　　　　　　　█▀'")
+print(Fore.RESET + "'▐░░░░░░░▒▒▐▒▒░░░░░░░▌  '"                + Fore.RESET + "'     ▀█▄　　　　　　█▀　　　▀█　　　▄▄▄▄█▀'")
+print(Fore.RESET + "'▐▒░░░░░▒▒▒▐▒▒▒░░░░░▒▌   '"               + Fore.RESET + "'        ▀█▄▄▄█▀　　　　　　▀█▄▄▄█▀'")
+print(Fore.RESET + "'.▀▄▒▒▒▒▒▄▀▒▀▄▒▒▒▒▒▄▀     '"              + Fore.RESET + "''")
+print(Fore.RESET + "'     ▀▀▀▀▀ ▀▀▀▀▀          '"             + Fore.RESET + "''")
+
+
+
+
+print(Fore.RED + "Powered By SEZER")
+print("")
+print("")
+# BÜTÜN KAYNAK KODLARI SEZER KARAOĞLAN'A AİTTİR
+
+k = input(Fore.GREEN + "ÇIKMAK İÇİN BİR TUŞA BAS")
+
+
